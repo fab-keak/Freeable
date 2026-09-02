41bddf24912603c7d813021b69c32a3f79d60e62
fab-keak <138788598+fab-keak@users.noreply.github.com>
2026-09-02 15:41:14 -0700

    Add X conversion tracking pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { IBM_Plex_Mono, Instrument_Serif, Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import './editor-pages.css';
 
@@ -58,6 +59,9 @@ export default function RootLayout({
         className={`${inter.variable} ${instrumentSerif.variable} ${ibmPlexMono.variable} antialiased`}
       >
         {children}
+        <Script id="x-conversion-tracking" strategy="afterInteractive">
+          {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments)},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','ofrz1');`}
+        </Script>
       </body>
     </html>
   );
